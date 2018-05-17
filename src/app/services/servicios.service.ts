44a118e7b6080fcfc4080557f829adff86f21e7e
guslalo <gf.renjifo@gmail.com>
2018-05-17 15:33:39 -0400

    merge

--- src/app/services/servicios.service.ts
+++ src/app/services/servicios.service.ts
@@ -5,8 +5,8 @@ import {of} from 'rxjs/observable/of';
 import 'rxjs/add/operator/map'
 
  
-// const url = 'https://rinnolab.cl'
-const url = 'http://localhost:8000'
+const url = 'https://rinnolab.cl'
+// const url = 'http://localhost:8000'
 
 @Injectable()
 export class FormService {
