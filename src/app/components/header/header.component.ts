4a660f57ebc2e83c3e0a7a8d7e0b8911403f9333
guslalo <gf.renjifo@gmail.com>
2018-05-22 14:06:02 -0400

    buscador header avance

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { FormService } from '../../services/servicios.service';
+import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
 import * as $ from 'jquery';
 
 @Component({
@@ -7,16 +10,49 @@ import * as $ from 'jquery';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() {
+  subscription;
+  constructor(private http : HttpClient, private FormsService: FormService,) {
 
 
 
    }
 
+   search = (text$: Observable<string>) =>
+   text$
+     .debounceTime(200)
+     .distinctUntilChanged()
+     .do((text) => console.log())
+     .switchMap(term =>
+       this.FormsService.getEmployeesList(term)  
+     );
+   formatter = (x: {rut: string, check_digit:string}) => x.rut + '-' + x.check_digit;
+
   ngOnInit() {
+    
        
   }
+
+    // Funcion para cargar toda la informacion del paciente seleccionado incluyendo los valores si ya tenia una atencion medica incompleta.
+    cargarDatos(currentEmployee){
+    
+      localStorage.setItem('employee', JSON.stringify(currentEmployee));
+      this.subscription = this.FormsService.trabajadoresInfo(currentEmployee.id).subscribe( 
+        data => {
+          for(let datos of data['results']){
+            //this.workingInformation = datos;
+            localStorage.setItem('workingInformation', JSON.stringify(datos));
+          } 
+          $("form.formNuevaAtencion *").prop("disabled",false);  
+          $("#nueva-btn-completar-discapacidad").attr("disabled", 'disabled');
+          $("#fila8 input").attr("disabled", 'disabled');  
+          $(".btnAceptar").attr("disabled", 'disabled');
+          $("#parrafo-caso").attr("disabled", 'disabled');            
+        },
+        error => {
+            console.log(<any>error);
+        }
+      );
+    }
 
 
 }
